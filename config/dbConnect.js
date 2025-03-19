3636202394587b1e9a7934aa0d342c68fc4e483d
Asmabi <138667886+Asmashajeer@users.noreply.github.com>
2025-03-19 10:58:39 +0400

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ const env =require('dotenv').config();
 
 const dbConnect = async ()=>{
  try{   
-       console.log((process.env.MONGODB_URI);
+       console.log(process.env.MONGODB_URI);
        const conn= await  mongoose.connect(process.env.MONGODB_URI);
   
         console.log('Database Connected!');        
